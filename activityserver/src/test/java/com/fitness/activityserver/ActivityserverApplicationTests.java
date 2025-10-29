c4e65f29116b12072d8d2f8db6ee846094ec4db9
Shivesh Pratap Mall <shiveshmall12499@gmail.com>
2025-10-29 23:19:47 +0530

    initial commit

---
+++ activityserver/src/test/java/com/fitness/activityserver/ActivityserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.activityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
